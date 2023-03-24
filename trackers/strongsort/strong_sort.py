78df41a061a134798f5007a7191f92ad6ae820b3
Andrea Zunino <andreazuna@hotmail.it>
2023-03-24 14:56:25 +0100

    Solving import issues due to installation

--- trackers/strongsort/strong_sort.py
+++ trackers/strongsort/strong_sort.py
@@ -6,11 +6,11 @@ import gdown
 from os.path import exists as file_exists, join
 import torchvision.transforms as transforms
 
-from sort.nn_matching import NearestNeighborDistanceMetric
-from sort.detection import Detection
-from sort.tracker import Tracker
+from trackers.strongsort.sort.nn_matching import NearestNeighborDistanceMetric
+from trackers.strongsort.sort.detection import Detection
+from trackers.strongsort.sort.tracker import Tracker
 
-from reid_multibackend import ReIDDetectMultiBackend
+from trackers.strongsort.reid_multibackend import ReIDDetectMultiBackend
 
 from yolov8.ultralytics.yolo.utils.ops import xyxy2xywh
 
