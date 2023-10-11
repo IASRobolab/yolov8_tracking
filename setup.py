ddf85b42cfd19344e4b86ed03e89c526f8d88fb7
Frollo <rollo.f96@gmail.com>
2023-10-11 16:55:16 +0200

    added install requires

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     data_files=[],
-    install_requires=[],
+    install_requires=[easydict, ultralytics==8.0.20],
     zip_safe=True,
     description='This package contains functions useful for ML algorithm use. It creates simple interface between some used AI algorithms.',
     license='GNU GENERAL PUBLIC LICENSE v3',
